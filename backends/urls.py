7f2579e155b09299e80b725cd3274c4b450c22ad
Joanna Gorska <gorska919@gmail.com>
2022-05-21 21:06:45 +0000

    Add migrations and minor fixes

--- backends/urls.py
+++ backends/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from home import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,4 +35,5 @@ urlpatterns = [
     path('games/canvas_art/', views.get_canvas_art, name='canvas_art'),
     path('games/game_5/', views.get_game_5, name='game_5'),
     path('games/game_6/', views.get_game_6, name='game_6'),
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
